804515118b879e35df9d9ebd7953eb911a54de45
Oguzhan <Oguzhan@DESKTOP-HSH38D0>
2024-07-07 16:01:38 +0300

    Malzeme veri tablosu oluşturuldu. Modul_Malzeme ekranında ki EKLE, SİL ve DÜZELT butonları için işlevler oluşturuldu. dataGridView1'in CellClick özelliği kullanıldı.

--- ERP_Projesi_V1.0/Formlar/Modul_Malzeme.Designer.cs
+++ ERP_Projesi_V1.0/Formlar/Modul_Malzeme.Designer.cs
@@ -29,6 +29,7 @@ namespace ERP_Projesi_V1._0
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -39,9 +40,17 @@ namespace ERP_Projesi_V1._0
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.kKP_V1DataSet = new ERP_Projesi_V1._0.KKP_V1DataSet();
+            this.malzemeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.malzemeTableAdapter = new ERP_Projesi_V1._0.KKP_V1DataSetTableAdapters.malzemeTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.adiDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.miktariDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kKP_V1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.malzemeBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -65,7 +74,7 @@ namespace ERP_Projesi_V1._0
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(180, 28);
+            this.label2.Location = new System.Drawing.Point(182, 28);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(84, 24);
             this.label2.TabIndex = 2;
@@ -73,7 +82,7 @@ namespace ERP_Projesi_V1._0
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(270, 30);
+            this.textBox2.Location = new System.Drawing.Point(272, 30);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 22);
             this.textBox2.TabIndex = 3;
@@ -86,27 +95,38 @@ namespace ERP_Projesi_V1._0
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(383, 69);
+            this.groupBox1.Size = new System.Drawing.Size(379, 69);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "mlazme bilgileri";
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.adiDataGridViewTextBoxColumn,
+            this.miktariDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.malzemeBindingSource;
             this.dataGridView1.Location = new System.Drawing.Point(12, 87);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(279, 306);
+            this.dataGridView1.Size = new System.Drawing.Size(275, 306);
             this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.button3);
             this.groupBox2.Controls.Add(this.button2);
             this.groupBox2.Controls.Add(this.button1);
-            this.groupBox2.Location = new System.Drawing.Point(297, 87);
+            this.groupBox2.Location = new System.Drawing.Point(293, 87);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(98, 151);
             this.groupBox2.TabIndex = 6;
@@ -122,6 +142,7 @@ namespace ERP_Projesi_V1._0
             this.button3.TabIndex = 2;
             this.button3.Text = "DÜZELT";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
@@ -132,6 +153,7 @@ namespace ERP_Projesi_V1._0
             this.button2.TabIndex = 1;
             this.button2.Text = "SİL";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
@@ -142,6 +164,48 @@ namespace ERP_Projesi_V1._0
             this.button1.TabIndex = 0;
             this.button1.Text = "EKLE";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // kKP_V1DataSet
+            // 
+            this.kKP_V1DataSet.DataSetName = "KKP_V1DataSet";
+            this.kKP_V1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // malzemeBindingSource
+            // 
+            this.malzemeBindingSource.DataMember = "malzeme";
+            this.malzemeBindingSource.DataSource = this.kKP_V1DataSet;
+            // 
+            // malzemeTableAdapter
+            // 
+            this.malzemeTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            this.idDataGridViewTextBoxColumn.Width = 50;
+            // 
+            // adiDataGridViewTextBoxColumn
+            // 
+            this.adiDataGridViewTextBoxColumn.DataPropertyName = "adi";
+            this.adiDataGridViewTextBoxColumn.HeaderText = "adi";
+            this.adiDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.adiDataGridViewTextBoxColumn.Name = "adiDataGridViewTextBoxColumn";
+            this.adiDataGridViewTextBoxColumn.ReadOnly = true;
+            this.adiDataGridViewTextBoxColumn.Width = 75;
+            // 
+            // miktariDataGridViewTextBoxColumn
+            // 
+            this.miktariDataGridViewTextBoxColumn.DataPropertyName = "miktari";
+            this.miktariDataGridViewTextBoxColumn.HeaderText = "miktari";
+            this.miktariDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.miktariDataGridViewTextBoxColumn.Name = "miktariDataGridViewTextBoxColumn";
+            this.miktariDataGridViewTextBoxColumn.ReadOnly = true;
+            this.miktariDataGridViewTextBoxColumn.Width = 75;
             // 
             // Modul_Malzeme
             // 
@@ -153,10 +217,13 @@ namespace ERP_Projesi_V1._0
             this.Controls.Add(this.groupBox2);
             this.Name = "Modul_Malzeme";
             this.Text = "MALZEME";
+            this.Load += new System.EventHandler(this.Modul_Malzeme_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.kKP_V1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.malzemeBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -173,5 +240,11 @@ namespace ERP_Projesi_V1._0
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private KKP_V1DataSet kKP_V1DataSet;
+        private System.Windows.Forms.BindingSource malzemeBindingSource;
+        private KKP_V1DataSetTableAdapters.malzemeTableAdapter malzemeTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn adiDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn miktariDataGridViewTextBoxColumn;
     }
 }
--- ERP_Projesi_V1.0/Formlar/Modul_Malzeme.cs
+++ ERP_Projesi_V1.0/Formlar/Modul_Malzeme.cs
@@ -7,15 +7,204 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace ERP_Projesi_V1._0
 {
     public partial class Modul_Malzeme : Form
     {
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-HSH38D0\\SQLEXPRESS;Initial Catalog=KKP_V1;Integrated Security=True");
+
         public Modul_Malzeme()
         {
             InitializeComponent();
+            listele();
+        }
+        private void Modul_Malzeme_Load(object sender, EventArgs e)
+        {
+            // TODO: Bu kod satırı 'kKP_V1DataSet.malzeme' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.malzemeTableAdapter.Fill(this.kKP_V1DataSet.malzeme);
+
         }
 
+        //EKLE butonu
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String adi;
+            int miktari;
+            try
+            {
+                adi = textBox1.Text.ToString();
+            }
+            catch (Exception hata)
+            {
+                adi = "";
+            }
+            try
+            {
+                miktari = int.Parse(textBox2.Text.ToString());
+            }
+            catch (Exception hata)
+            {
+
+                miktari = 0;
+            }
+            if (adi != "")
+            {
+                if (miktari > 0)
+                {
+                    baglanti.Open();
+                    String sqlKomutu = "INSERT INTO malzeme(adi, miktari) " +
+                        "VALUES('" + adi + "', '" + miktari + "')";
+                    SqlCommand komut = new SqlCommand(sqlKomutu, baglanti);
+                    int i = komut.ExecuteNonQuery();
+                    baglanti.Close();
+                    if (i > 0)
+                        Console.Out.WriteLine("Ürün başarılı bir şekilde eklendi.");
+                    else
+                        MessageBox.Show("Ürün eklenemedi.");
+                }
+                else
+                {
+                    MessageBox.Show("Miktarı boş bırakılamaz.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Adı boş bırakılamaz.");
+            }
+            listele();
+            temizle();
+        }
+        //SİL butonu
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String adi;
+            int miktari;
+            try
+            {
+                adi = textBox1.Text.ToString();
+            }
+            catch (Exception hata)
+            {
+                adi = "";
+            }
+            try
+            {
+                miktari = int.Parse(textBox2.Text.ToString());
+            }
+            catch (Exception hata)
+            {
+
+                miktari = 0;
+            }
+            if (adi != "")
+            {
+                if (miktari > 0)
+                {
+                    baglanti.Open();
+                    String sqlKomutu = "DELETE FROM malzeme WHERE adi='" + adi + "'";
+                    SqlCommand komut = new SqlCommand(sqlKomutu, baglanti);
+                    int i = komut.ExecuteNonQuery();
+                    baglanti.Close();
+                    if (i > 0)
+                        Console.Out.WriteLine("Ürün başarılı bir şekilde silindi.");
+                    else
+                        MessageBox.Show("Ürün silinemedi.");
+                }
+                else
+                {
+                    MessageBox.Show("Miktarı boş bırakılamaz.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Adı boş bırakılamaz.");
+            }
+            listele();
+            temizle();
+        }
+        int id;
+        //DÜZELT botonu
+        private void button3_Click(object sender, EventArgs e)
+        {
+            {
+                String adi;
+                int miktari;
+                try
+                {
+                    adi = textBox1.Text.ToString();
+                }
+                catch (Exception hata)
+                {
+                    adi = "";
+                }
+                try
+                {
+                    miktari = int.Parse(textBox2.Text.ToString());
+                }
+                catch (Exception hata)
+                {
+
+                    miktari = 0;
+                }
+                if (id > 0)
+                {
+                    if (adi != "")
+                    {
+                        if (miktari > 0)
+                        {
+                            baglanti.Open();
+                            String sqlKomutu = "UPDATE malzeme SET adi='" + adi + "', miktari='" + miktari + "' WHERE id='" + id + "'";
+                            SqlCommand komut = new SqlCommand(sqlKomutu, baglanti);
+                            int i = komut.ExecuteNonQuery();
+                            baglanti.Close();
+                            if (i > 0)
+                                Console.Out.WriteLine("Ürün başarılı bir şekilde düzeltildi.");
+                            else
+                                MessageBox.Show("Ürün düzeltilemedi.");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Miktarı boş bırakılamaz.");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Adı boş bırakılamaz.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Listeden bir ürün seçin.");
+                }
+
+                listele();
+                temizle();
+            }
+        }
+
+        public void listele()
+        {
+            baglanti.Open();
+            String sqlKomutu = "SELECT * FROM malzeme";
+            SqlCommand komut = new SqlCommand(sqlKomutu, baglanti);
+            SqlDataAdapter da = new SqlDataAdapter(komut);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            baglanti.Close();
+        }
+        public void temizle()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            id = int.Parse(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            textBox1.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+        }
     }
 }
