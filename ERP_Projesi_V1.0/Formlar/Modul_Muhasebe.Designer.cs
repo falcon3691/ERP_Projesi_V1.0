c974e6cf8d9ff4071528d4a69d34c46811e40ee7
Oguzhan <Oguzhan@DESKTOP-HSH38D0>
2024-07-12 12:21:01 +0300

    Modul_Muhasebe ekranı tasarımı değiştirildi.

--- ERP_Projesi_V1.0/Formlar/Modul_Muhasebe.Designer.cs
+++ ERP_Projesi_V1.0/Formlar/Modul_Muhasebe.Designer.cs
@@ -51,7 +51,7 @@ namespace ERP_Projesi_V1._0
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(12, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(497, 108);
+            this.groupBox1.Size = new System.Drawing.Size(412, 108);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "tarih seçimi";
@@ -59,7 +59,7 @@ namespace ERP_Projesi_V1._0
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button1.Location = new System.Drawing.Point(147, 61);
+            this.button1.Location = new System.Drawing.Point(195, 61);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(112, 37);
             this.button1.TabIndex = 5;
@@ -71,7 +71,7 @@ namespace ERP_Projesi_V1._0
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(265, 33);
+            this.label2.Location = new System.Drawing.Point(176, 31);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(15, 20);
             this.label2.TabIndex = 4;
@@ -92,9 +92,9 @@ namespace ERP_Projesi_V1._0
             // dateTimePicker2
             // 
             this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker2.Location = new System.Drawing.Point(286, 33);
+            this.dateTimePicker2.Location = new System.Drawing.Point(197, 33);
             this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker2.Size = new System.Drawing.Size(110, 22);
             this.dateTimePicker2.TabIndex = 2;
             this.dateTimePicker2.Visible = false;
             this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
@@ -104,7 +104,7 @@ namespace ERP_Projesi_V1._0
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.dateTimePicker1.Location = new System.Drawing.Point(59, 33);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.Size = new System.Drawing.Size(112, 22);
             this.dateTimePicker1.TabIndex = 1;
             this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
@@ -123,15 +123,16 @@ namespace ERP_Projesi_V1._0
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(11, 119);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(498, 299);
+            this.dataGridView1.Size = new System.Drawing.Size(413, 299);
             this.dataGridView1.TabIndex = 1;
             // 
             // Modul_Muhasebe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(523, 426);
+            this.ClientSize = new System.Drawing.Size(437, 426);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.groupBox1);
             this.Name = "Modul_Muhasebe";
